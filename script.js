b647111df69a0ee427c2dd5d2fb2988704979bb9
luizasilva18clear <luizagoncalves7@gmail.com>
2021-05-27 18:09:20 -0300

    Desafio 5

---
+++ script.js
@@ -0,0 +1,12 @@
+let turma4 =["Luiza", "Leandro", "Laila", "Bea", "Luiz"]
+turma4.unshift("Paola")
+turma4.pop()
+turma4.push("Dandara")
+turma4.push("Pam")
+turma4.shift()
+
+console.log(turma4)
+
+const numbers = [7,5,6,3,8,9,2,1,4]
+numbers.sort()
+console.log(numbers)
